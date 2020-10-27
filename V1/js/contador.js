4621e92a47a9fd49409c78150ccbe60a462cdb80
KH0RE <52050442+KH0RE@users.noreply.github.com>
2020-10-26 19:45:42 -0500

    guiaEcu

---
+++ V1/js/contador.js
@@ -0,0 +1,9 @@
+let counter= 0;
+
+function counting(){
+    document.getElementById("counting").innerHTML = ++counter;
+    document.getElementById("counting2").innerHTML = +counter;
+    document.getElementById("counting3").innerHTML = +counter;
+    document.getElementById("counting4").innerHTML = +counter;
+    document.getElementById("counting5").innerHTML = +counter;
+}
